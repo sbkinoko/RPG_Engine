d573f69f30c187e56712aa26e20ce8acfcdf45e7
sbkinoko <136720653+sbkinoko@users.noreply.github.com>
2023-07-28 22:14:30 +0900

    hiltの導入

---
+++ app/src/main/java/com/sbkinoko/sbkinokorpg/app/SBKinokoRPGApp.java
@@ -0,0 +1,10 @@
+package com.sbkinoko.sbkinokorpg.app;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class SBKinokoRPGApp extends Application {
+
+}
