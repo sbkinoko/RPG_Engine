6082252f349175cc88927dd140cb3feeedf9bd3b
sbkinoko <136720653+sbkinoko@users.noreply.github.com>
2023-08-05 15:31:31 +0900

    アノテーションを追加

--- app/src/main/java/com/sbkinoko/sbkinokorpg/application/SbKinokoRPGApplication.java
+++ app/src/main/java/com/sbkinoko/sbkinokorpg/application/SbKinokoRPGApplication.java
@@ -2,5 +2,8 @@ package com.sbkinoko.sbkinokorpg.application;
 
 import android.app.Application;
 
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
 public class SbKinokoRPGApplication extends Application {
 }
